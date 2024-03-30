c2ea098d5237f44ea16492f987b92d4a5c48dadd
Gaurav-246 <gauravnvn246@gmail.com>
2024-03-30 22:40:09 +0530

    all done first stage

--- ass_3/src/download.py
+++ ass_3/src/download.py
@@ -32,7 +32,7 @@ def download_dataset(n_loc, csv_urls, download_path):
     for csv_url in random_csv_urls:
         filename = os.path.join(download_path, csv_url.split('/')[-1])
         filenames.append(filename)
-        print('Downloading file ', i+1,' : ', filename[-14:])
+        print('Downloading file ', i+1,' : ', filename[-15:])
         response = requests.get(csv_url)
         with open(filename, 'wb') as f:                       # Downloads the files to the download_path
             f.write(response.content)        
@@ -63,12 +63,12 @@ def validate_dataset(filenames):
         
         for common_col in common_col_names:
             if df['Daily'+common_col].count()!=0 and df['Monthly'+common_col].count()!=0:       # Store only valid csv files
-                print('The file ', filename[-14:], ' is valid with daily and monthly data existing for the column : ', common_col)
+                print('The file ', filename[-15:], ' is valid with daily and monthly data existing for the column : ', common_col)
                 valid_csv_files.append(filename)             
             else:
-                print('The file ', filename[-14:], ' is invalid')
+                print('The file ', filename[-15:], ' is invalid')
                 os.remove(filename)                                                             # Delete the invlid csv files
-                print('File ', filename[-14:], 'deleted')
+                print('File ', filename[-15:], 'deleted')
 
     print('Total number of sampled files : ', len(filenames))
     print('Number of valid files are : ', len(valid_csv_files))
@@ -78,16 +78,20 @@ def validate_dataset(filenames):
 #                          MAIN FUNCTION
 #########################################################################
 def main():
-    params = yaml.safe_load(open("params.yaml"))["download"]
+    params = yaml.safe_load(open("/home/laog/CS5830_Assignment_3/params.yaml"))["download"]
     
     year = params["year"]
     n_loc = params["n_loc"]
     base_url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/'
-    csv_download_path = r'\\wsl.localhost\Ubuntu-22.04\home\laog\CS5830_Assignment_3\ass_3\data'
+    csv_download_path = '/home/laog/CS5830_Assignment_3/ass_3/data'
     all_csv_urls = extract_csv_urls(base_url, year)                                 
 
     num_of_valid_files = 0
     final_valid_files = []
+    if not os.path.exists (csv_download_path) :
+        os.makedirs(csv_download_path)
+        print('Created data dir')
+
     while num_of_valid_files < n_loc:                           # Keep sampling till you get the required number of valid files
 
         sampled_files = download_dataset(n_loc, all_csv_urls, csv_download_path)
